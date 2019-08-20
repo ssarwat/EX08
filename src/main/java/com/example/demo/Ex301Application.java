cd9326864d17b7d420596d997897f7a6e18033e1
ssarwat <sumaiyah_sarwat@yahoo.com>
2019-08-20 13:36:44 -0400

    saving data to database

---
+++ src/main/java/com/example/demo/Ex301Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex301Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex301Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String Fname;
+    private String Lname;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String fname) {
+        Fname = fname;
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+
+    public void setLname(String lname) {
+        Lname = lname;
+    }
+}
